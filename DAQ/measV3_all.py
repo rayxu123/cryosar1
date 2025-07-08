e5d66524fbca9e29105916c4426c105f6ebb82b5
cryoPC <rayxu123@gmail.com>
2025-07-08 18:07:24 -0400

    increase delay

--- DAQ/measV3_all.py
+++ DAQ/measV3_all.py
@@ -160,7 +160,7 @@ def sineSweep(freq, initAmp, dataDir):
         awg.initSine(freq, awgAmp)
         awg.setOutput(True)
         # Let signals settle
-        time.sleep(1.2)
+        time.sleep(1.4)
         
         # Take data
         data, valid, datar2 = fpga.takeData("data", bipolar=False, printBinary=False, weighting=cal.weights, mult=1)
